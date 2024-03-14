67fdf3c479045860542b606497596180b5642800
Suyi <thonatos.yang@gmail.com>
2024-03-15 01:20:03 +0800

    feat(rsshub): update sidebar style

--- packages/apps/rsshub/src/layout.tsx
+++ packages/apps/rsshub/src/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Sidebar, DarkThemeToggle, Flowbite } from 'flowbite-react';
+import { Sidebar, DarkThemeToggle, Flowbite, Button } from 'flowbite-react';
 import {
   HiArrowSmRight,
   HiChartPie,
@@ -15,39 +15,28 @@ export const Layout: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
     <Flowbite>
       <div className="flex flex-row gap-2">
         <div className="fixed top-0 left-0 h-screen">
-          <Sidebar aria-label="Default sidebar example">
+          <Sidebar aria-label="sidebar" collapsed={true}>
             <Sidebar.Items>
               <Sidebar.ItemGroup>
-                <Sidebar.Item href="#" icon={HiChartPie}>
-                  Dashboard
-                </Sidebar.Item>
                 <Sidebar.Item href="#" icon={HiViewBoards} label="Pro" labelColor="dark">
                   Kanban
                 </Sidebar.Item>
-                <Sidebar.Item href="#" icon={HiInbox} label="3">
-                  Inbox
-                </Sidebar.Item>
                 <Sidebar.Item href="#" icon={HiUser}>
-                  Users
-                </Sidebar.Item>
-                <Sidebar.Item href="#" icon={HiShoppingBag}>
-                  Products
-                </Sidebar.Item>
-                <Sidebar.Item href="#" icon={HiArrowSmRight}>
-                  Sign In
+                  User
                 </Sidebar.Item>
                 <Sidebar.Item href="#" icon={HiTable}>
                   Sign Up
                 </Sidebar.Item>
               </Sidebar.ItemGroup>
+              <Sidebar.ItemGroup>
+                <div>
+                  <DarkThemeToggle />
+                </div>
+              </Sidebar.ItemGroup>
             </Sidebar.Items>
           </Sidebar>
         </div>
-        <div className="flex-1">
-          <DarkThemeToggle />
-
-          {children}
-        </div>
+        <div className="flex-1">{children}</div>
       </div>
     </Flowbite>
   );
