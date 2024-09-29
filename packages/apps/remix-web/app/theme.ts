8974f0c422888afe6f866d683f941851d6de4583
Suyi <thonatos.yang@gmail.com>
2024-09-29 02:31:06 +0000

    chore: remove remix-web and rsshub-app

--- packages/apps/remix-web/app/theme.ts
+++
@@ -1,9 +0,0 @@
-import type { CustomFlowbiteTheme } from 'flowbite-react';
-
-export const customTheme: CustomFlowbiteTheme = {
-  // button: {
-  //   color: {
-  //     primary: 'bg-red-500 hover:bg-red-600',
-  //   },
-  // },
-};
--- packages/apps/rsshub-app/src/components/Logo/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import styles from './index.module.css';
-
-export const Logo: React.FC<{}> = () => {
-  return <span className={styles.logo}>ρV</span>;
-};
