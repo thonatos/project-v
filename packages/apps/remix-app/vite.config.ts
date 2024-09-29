4d6f49afbf03e775970f4be218267a729a0e3402
Suyi <thonatos.yang@gmail.com>
2024-09-29 07:54:41 +0000

    chore(remix-app): update vercel config

--- packages/apps/remix-app/vite.config.ts
+++ packages/apps/remix-app/vite.config.ts
@@ -1,17 +1,20 @@
 import { vitePlugin as remix } from '@remix-run/dev';
+import { installGlobals } from '@remix-run/node';
 import { defineConfig } from 'vite';
 import { vercelPreset } from '@vercel/remix/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+
+installGlobals();
 
 export default defineConfig({
   plugins: [
     remix({
       presets: [vercelPreset()],
-      // future: {
-      //   v3_fetcherPersist: true,
-      //   v3_relativeSplatPath: true,
-      //   v3_throwAbortReason: true,
-      // },
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+      },
     }),
     tsconfigPaths(),
   ],
