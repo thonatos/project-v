bf36e15c0cf927cd7bfe9de4e36409466680bb49
Suyi <thonatos.yang@gmail.com>
2024-10-06 10:45:15 +0800

    feat(remix-app): fix build warning

--- packages/apps/remix-app/app/routes/action.set-theme.ts
+++ packages/apps/remix-app/app/routes/action.set-theme.ts
@@ -2,3 +2,5 @@ import { createThemeAction } from 'remix-themes';
 import { themeSessionResolver } from '../sessions.server';
 
 export const action = createThemeAction(themeSessionResolver);
+
+export default action;
