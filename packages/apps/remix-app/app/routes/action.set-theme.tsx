5961a5231503df09f6f8f8bcc995c54d2e7e3cfa
Suyi <thonatos.yang@gmail.com>
2024-10-08 13:22:55 +0000

    chore: fix warnings

--- packages/apps/remix-app/app/routes/action.set-theme.tsx
+++ packages/apps/remix-app/app/routes/action.set-theme.tsx
@@ -2,3 +2,9 @@ import { createThemeAction } from 'remix-themes';
 import { themeSessionResolver } from '../sessions.server';
 
 export const action = createThemeAction(themeSessionResolver);
+
+export const ActionSetThemePage: React.FC<{}> = () => {
+  return null;
+};
+
+export default ActionSetThemePage;
