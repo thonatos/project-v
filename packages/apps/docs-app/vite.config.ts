3cd7e18fc0d9ed9422c33dcd4a7baafadb956b1f
Suyi <thonatos.yang@gmail.com>
2026-04-28 23:40:32 +0800

    chore(docs-app): add wrangler config

--- packages/apps/docs-app/vite.config.ts
+++ packages/apps/docs-app/vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
+import { cloudflare } from '@cloudflare/vite-plugin';
 import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    cloudflare({
+      viteEnvironment: {
+        name: 'docs-app',
+      },
+    }),
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+  ],
 });
